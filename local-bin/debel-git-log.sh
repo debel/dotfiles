29d6b0b75e21344192078c732b632c06bc8c89cf
Mihail Mikov <m.mikoff@gmail.com>
2025-09-17 23:05:14 +0300

    updated arch configs

--- local-bin/debel-git-log.sh
+++ local-bin/debel-git-log.sh
@@ -1,7 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+
+set -euo pipefail
 
 git log --graph --color=always \
-  --format='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' "$@" \
-  | fzf --ansi --multi --reverse \
-  --preview 'echo {} | sed -n "s/^.* \([0-9a-f]\{5,40\}\) -.*$/\1/p" | xargs git show --abbrev-commit --date=human --stat -p --color=always' \
-  | sed -n "s/^.* \([0-9a-f]\{5,40\}\) -.*$/\1/p"  | xargs
+  --format='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' "$@" |
+  fzf --ansi --multi --reverse \
+    --preview 'echo {} | sed -n "s/^.* \([0-9a-f]\{5,40\}\) -.*$/\1/p" | xargs git show --abbrev-commit --date=human --stat -p --color=always' |
+  sed -n "s/^.* \([0-9a-f]\{5,40\}\) -.*$/\1/p" | xargs
